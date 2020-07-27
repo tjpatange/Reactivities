37badfeb4dca5456a51776cf85ef3f9274338af4
Tejashri <tejashripatange@gmail.com>
2020-07-27 10:59:10 +0200

    End of Section 5

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import { ActivityList } from "./ActivityList";
+import { ActivityDetails } from "./details/ActivityDetails";
+import { ActivityForm } from "../form/ActivityForm";
+interface IProps {
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
+  setSelectedActivity: (activity: IActivity | null) => void;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+  deleteActivity: (id: string) => void;
+}
+
+export const ActivityDashboard: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  selectedActivity,
+  editMode,
+  setEditMode,
+  setSelectedActivity,
+  createActivity,
+  editActivity,
+  deleteActivity,
+}) => {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          selectedActivity={selectedActivity}
+          deleteActivity={deleteActivity}
+        ></ActivityList>
+      </Grid.Column>
+      <Grid.Column widescreen={6}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            setEditMode={setEditMode}
+            setSelectedActivity={setSelectedActivity}
+          ></ActivityDetails>
+        )}
+        {editMode && (
+          <ActivityForm
+            key={(selectedActivity && selectedActivity.id) || 0}
+            setEditMode={setEditMode}
+            activity={selectedActivity!}
+            createActivity={createActivity}
+            editActivity={editActivity}
+          ></ActivityForm>
+        )}
+      </Grid.Column>
+    </Grid>
+  );
+};
---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,105 @@
+import React, { useState, FormEvent } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+import {v4 as uuid} from 'uuid';
+interface IProps {
+  setEditMode: (editMode: boolean) => void;
+  activity: IActivity;
+  createActivity: (activity: IActivity) => void;
+  editActivity: (activity: IActivity) => void;
+}
+export const ActivityForm: React.FC<IProps> = ({
+  setEditMode,
+  activity: initialFormState,
+  createActivity,
+  editActivity
+}) => {
+  const initializeForm = () => {
+    if (initialFormState) {
+      return initialFormState;
+    } else {
+      return {
+        id: "",
+        title: "",
+        category: "",
+        description: "",
+        date: "",
+        venue: "",
+        city: "",
+      };
+    }
+  };
+  const [activity, setActivity] = useState<IActivity>(initializeForm);
+
+  const handleSubmit = () => {
+    console.log(activity);
+    if(activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid()
+      }
+      createActivity(newActivity);
+    } else {
+      editActivity(activity);
+    }
+  };
+
+  const handleInputChange = (
+    event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.currentTarget;
+    setActivity({ ...activity, [name]: value });
+  };
+
+  return (
+    <Segment clearing>
+      <Form onSubmit={handleSubmit}>
+        <Form.Input
+          onChange={handleInputChange}
+          placeholder="Title"
+          name="title"
+          value={activity.title}
+        />
+        <Form.TextArea
+          rows={2}
+          onChange={handleInputChange}
+          placeholder="Description"
+          name="description"
+          value={activity.description}
+        />
+        <Form.Input
+          onChange={handleInputChange}
+          placeholder="Category"
+          name="category"
+          value={activity.category}
+        />
+        <Form.Input
+          onChange={handleInputChange}
+          type="datetime-local"
+          placeholder="Date"
+          name="date"
+          value={activity.date}
+        />
+        <Form.Input
+          onChange={handleInputChange}
+          name="city"
+          placeholder="City"
+          value={activity.city}
+        />
+        <Form.Input
+          onChange={handleInputChange}
+          name="venue"
+          placeholder="Venue"
+          value={activity.venue}
+        />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          onClick={() => setEditMode(false)}
+          floated="right"
+          type="button"
+          content="Cancel"
+        />
+      </Form>
+    </Segment>
+  );
+};
