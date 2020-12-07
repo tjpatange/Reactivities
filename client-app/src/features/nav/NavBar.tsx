bfc8a5420d3ce0f401dfd66164c91882a9cb478c
Tejashri <tejashripatange@gmail.com>
2020-12-07 20:51:12 +0100

    End of Session 9

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,20 +1,23 @@
-import React, { useContext } from "react";
-import { Menu, Container, Button } from "semantic-ui-react";
-import ActivityStore from '../../app/stores/activityStore';
-import { observer } from "mobx-react-lite";
-export const NavBar: React.FC = () => {
-  const activityStore = useContext(ActivityStore);
-  const {openCreateForm} = activityStore;
+import React from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import { NavLink } from 'react-router-dom';
+
+const NavBar: React.FC = () => {
   return (
-    <Menu fixed="top" inverted>
+    <Menu fixed='top' inverted>
       <Container>
-        <Menu.Item>
-            <img src='/assets/logo.png' alt='logo' style={{marginRight: '10px'}}></img>
-            Reactivities
+        <Menu.Item header as={NavLink} exact to='/'>
+          <img src='/assets/logo.png' alt='logo' style={{ marginRight: 10 }} />
+          Reactivities
         </Menu.Item>
-        <Menu.Item name="messages" />
+        <Menu.Item name='Activities' as={NavLink} to='/activities' />
         <Menu.Item>
-            <Button onClick={openCreateForm} positive content='Create Activity'></Button>
+          <Button
+            as={NavLink} to='/createActivity'
+            positive
+            content='Create Activity'
+          />
         </Menu.Item>
       </Container>
     </Menu>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 ReactDOM.render(
-    <App />,
+  <BrowserRouter>
+  <ScrollToTop>
+  <App />
+  </ScrollToTop>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
